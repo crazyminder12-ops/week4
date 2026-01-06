6693cd50dc92e761707ed3010b4578e32918d2f9
crazyminder12-ops <crazyminder12@gmail.com>
2026-01-06 10:42:01 +0500

    Update Product.java

--- Product.java
+++ Product.java
@@ -2,14 +2,34 @@ public class Product {
 
     protected int id;
     protected String name;
-    protected double price;
-    protected int quantity;
+    private double price;
+    private int quantity;
 
     public Product(int id, String name, double price, int quantity) {
         this.id = id;
         this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+        setPrice(price);
+        setQuantity(quantity);
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        }
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity >= 0) {
+            this.quantity = quantity;
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public void showInfo() {
@@ -18,14 +38,5 @@ public class Product {
 
     public String getType() {
         return "Product";
-    }
-
-    public boolean inStock() {
-        return quantity > 0;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + getType() + "] " + name + " (" + price + " KZT)";
     }
 }
